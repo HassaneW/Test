ee236fb0b8831388f795c4b90f267d6e61afc433
HassaneW <wandianga33@gmail.com>
2024-05-21 18:18:22 +0000

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 7MAKSACOD PC on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
